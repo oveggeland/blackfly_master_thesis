285858ab640db7e1a2b0082e0cf87dd6c76fc593
Oskar Veggeland <oveggeland@gmail.com>
2021-11-12 21:16:29 +0100

    remove devel stuff

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oveggeland/Desktop/my_ws/src
-# Build directory: /home/oveggeland/Desktop/my_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("blackfly_nodelet")
